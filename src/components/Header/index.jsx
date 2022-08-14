26db2f38d38445581d0a6aa247e2d8637372cb83
Matheus-daSilva <matheus2001silva@live.com>
2022-08-14 20:39:14 -0300

    add: header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,57 @@
+import * as S from "./styles"
+import { useNavigate } from "react-router-dom"
+import { FiAlignJustify, FiChevronDown } from "react-icons/fi"
+import abelha from "../../img/abelha.png"
+import { useState } from "react"
+
+export default function Header() {
+    const [show, setShow] = useState(false)
+    const navigate = useNavigate()
+
+    const storage = localStorage.getItem("token")
+
+    if (!show) {
+        return (
+            <>
+                <S.Header>
+                    <S.Logo onClick={() => navigate("/")}>
+                        <S.Image src={abelha} />
+                        <S.Title>Abelhas Sem Ferrão</S.Title>
+                    </S.Logo>
+                    <S.Icon onClick={() => setShow(!show)}>
+                        < FiAlignJustify color="#ffffff" size={30} />
+                    </S.Icon>
+                    <S.Pages2>
+                        <S.P2>Home</S.P2>
+                        <S.P2>Fórum</S.P2>
+                        <S.P2>Games</S.P2>
+                        <S.P2>{!storage ? "Login" : "Logout"}</S.P2>
+                    </S.Pages2>
+                </S.Header>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <S.Header>
+                    <S.Logo onClick={() => navigate("/")}>
+                        <S.Image src={abelha} />
+                        <S.Title>Abelhas Sem Ferrão</S.Title>
+                    </S.Logo>
+                    <S.Icon onClick={() => setShow(!show)}>
+                        < FiChevronDown color="#ffffff" size={30} />
+                    </S.Icon>
+                </S.Header>
+                <S.Body>
+                    <S.Pages>
+                        <S.P>Home</S.P>
+                        <S.P>Fórum</S.P>
+                        <S.P>Games</S.P>
+                        <S.P>{!storage ? "Login" : "Logout"}</S.P>
+                    </S.Pages>
+                </S.Body>
+            </>
+        )
+
+    }
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,91 @@
+import styled from "styled-components"
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #f7d88b;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    padding-left: 25px;
+    padding-right: 25px;
+    height: 60px;
+    z-index: 3;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+`
+
+export const Image = styled.img`
+    height: 40px;
+    width: 40px;
+    z-index: 2;
+`
+
+export const Title = styled.h1`
+    font-family: 'Varela Round', sans-serif;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    color: #ffffff;
+    font-size: 15px;
+`
+
+export const Logo = styled.div`
+    width: 200px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+`
+
+export const Icon = styled.div`
+    @media (min-width: 768px) {
+        display: none;
+    }
+`
+
+export const Body = styled.main`
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    rigth: 0px;
+    display: flex;
+    justify-content: center;
+    background: #f7d88b;
+`
+
+export const Pages = styled.div`
+    display: flex;
+    flex-direction: column;
+    aling-items: center;
+    margin-top: 100px;
+
+`
+
+export const Pages2 = styled.div`
+    display: none;
+
+    @media (min-width: 768px) {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 200px;
+        margin-rigth: 20px;
+    }
+`
+
+export const P = styled.p`
+    font-family: 'Varela Round', sans-serif;
+    color: #ffffff;
+    font-size: 18px;
+`
+
+export const P2 = styled.p`
+    font-family: 'Varela Round', sans-serif;
+    color: #ffffff;
+    font-size: 15px;
+`
