929176c4f468de15953faf371b77445d1d8f4723
Matheus-daSilva <matheus2001silva@live.com>
2022-08-17 20:18:52 -0300

    add: home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,24 @@
+import * as S from "./styles"
+import abelha from "../../assets/img/abelha-gif.gif"
 import Header from "../../components/Header"
 
 
 export default function Home() {
     return (
         <>
-            <Header />
+            <S.Container>
+                <Header />
+                <S.H1>SOBRE NÓS</S.H1>
+                <S.Text>
+                    Nosso propósito é levar conhecimento sobre abelhas nativas para as famílias brasileiras que desejam iniciar na apicultura
+                    e na meliponicultura amadora. Possuímos um fórum onde nossos usuários podem postar dúvidas, artigos e links.
+                </S.Text>
+                <S.Text>
+                    Também estamos desenvolvendo uma seção de games para as crianças aprenderem sobre a importância desses animais no nosso dia
+                    a dia.
+                </S.Text>
+                <S.Img src={abelha} />
+            </S.Container>
         </>
     )
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 1;
+`
+
+export const H1 = styled.h1`
+    font-family: 'Varela Round', sans-serif;
+    display: flex;
+    text-align: center;
+    font-size: 36px;
+    weight: 400;
+    color: #464d59;
+    margin-top: 100px;
+`
+
+export const Text = styled.p`
+    font-family: 'Nunito', sans-serif;
+    text-align: justify;
+    font-size: 22px;
+    line-height: 26px;
+    weight: 400;
+    color: #464d59;
+    width: 330px;
+    margin-top: 30px;
+
+    @media (min-width: 768px) {
+        font-size: 28px;
+        weight: 400;
+        color: #464d59;
+        width: 600px;
+        margin-top: 30px;
+    }
+`
+
+export const Img = styled.img`
+    width: 200px;
+    margin-top: 10px;
+`
